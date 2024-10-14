c23065c771a32df8c42e86a7a4ac740f2717b387
Dmitrii Rogulin <dima.rogulin@gmail.com>
2024-10-14 16:55:45 +0300

    Add chip8

---
+++ task/chip8/src/lib.rs
@@ -0,0 +1,16 @@
+#![forbid(unsafe_code)]
+#![no_std]
+
+mod data;
+mod error;
+mod image;
+mod interpreter;
+mod managed_interpreter;
+mod platform;
+
+pub use data::*;
+pub use error::*;
+pub use image::*;
+pub use interpreter::*;
+pub use managed_interpreter::*;
+pub use platform::*;
