823a7b573bb1f3c9559f2801da026f9b468e105c
zzucch <nikitamilovatskiy@gmail.com>
2024-10-16 01:18:45 +0300

    feat(pstack): solve the task

--- task/pstack/src/lib.rs
+++ task/pstack/src/lib.rs
@@ -2,52 +2,90 @@
 
 use std::rc::Rc;
 
+struct Node<T> {
+    data: Rc<T>,
+    next: Option<Rc<Node<T>>>,
+}
+
 pub struct PStack<T> {
-    // TODO: your code here.
+    head: Option<Rc<Node<T>>>,
+    size: usize,
 }
 
 impl<T> Default for PStack<T> {
     fn default() -> Self {
-        // TODO: your code here.
-        unimplemented!()
+        Self::new()
     }
 }
 
 impl<T> Clone for PStack<T> {
     fn clone(&self) -> Self {
-        // TODO: your code here.
-        unimplemented!()
+        PStack {
+            head: self.head.clone(),
+            size: self.size,
+        }
     }
 }
 
 impl<T> PStack<T> {
     pub fn new() -> Self {
-        // TODO: your code here.
-        unimplemented!()
+        Self {
+            head: None,
+            size: 0,
+        }
     }
 
     pub fn push(&self, value: T) -> Self {
-        // TODO: your code here.
-        unimplemented!()
+        PStack {
+            head: Some(Rc::new(Node {
+                data: Rc::new(value),
+                next: self.head.clone(),
+            })),
+            size: self.size + 1,
+        }
     }
 
     pub fn pop(&self) -> Option<(Rc<T>, Self)> {
-        // TODO: your code here.
-        unimplemented!()
+        self.head.as_ref().map(|head| {
+            (
+                Rc::clone(&head.data),
+                PStack {
+                    head: head.next.clone(),
+                    size: self.size - 1,
+                },
+            )
+        })
     }
 
     pub fn len(&self) -> usize {
-        // TODO: your code here.
-        unimplemented!()
+        self.size
     }
 
     pub fn is_empty(&self) -> bool {
-        // TODO: your code here.
-        unimplemented!()
+        self.size == 0
     }
 
     pub fn iter(&self) -> impl Iterator<Item = Rc<T>> {
-        // TODO: your code here.
-        unimplemented!()
+        PStackIterator {
+            current: self.head.clone(),
+        }
+    }
+}
+
+struct PStackIterator<T> {
+    current: Option<Rc<Node<T>>>,
+}
+
+impl<T> Iterator for PStackIterator<T> {
+    type Item = Rc<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.current.clone() {
+            Some(head) => {
+                self.current = head.next.clone();
+                Some(head.data.clone())
+            }
+            None => None,
+        }
     }
 }
