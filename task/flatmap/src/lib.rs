145916c2cf7c45e4aff909ffa16ddd35531754c2
zzucch <nikitamilovatskiy@gmail.com>
2024-10-16 14:04:37 +0300

    feat(flatmap): solve the task

--- task/flatmap/src/lib.rs
+++ task/flatmap/src/lib.rs
@@ -9,53 +9,135 @@ pub struct FlatMap<K, V>(Vec<(K, V)>);
 
 impl<K: Ord, V> FlatMap<K, V> {
     pub fn new() -> Self {
-        // TODO: your code here.
-        unimplemented!()
+        Self(Vec::new())
     }
 
     pub fn len(&self) -> usize {
-        // TODO: your code here.
-        unimplemented!()
+        self.0.len()
     }
 
     pub fn is_empty(&self) -> bool {
-        // TODO: your code here.
-        unimplemented!()
+        self.0.is_empty()
     }
 
     pub fn capacity(&self) -> usize {
-        // TODO: your code here.
-        unimplemented!()
+        self.0.capacity()
     }
 
     pub fn as_slice(&self) -> &[(K, V)] {
-        // TODO: your code here.
-        unimplemented!()
+        &self.0
     }
 
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
-        // Returns None if key was not present, or Some(prev_value) if it was.
-        // TODO: your code here.
-        unimplemented!()
+        match self.find(&key) {
+            Ok(index) => {
+                let (_, prev_value) = &mut self.0[index];
+                let prev_value = std::mem::replace(prev_value, value);
+
+                Some(prev_value)
+            }
+            Err(index) => {
+                self.0.insert(index, (key, value));
+                None
+            }
+        }
     }
 
-    // pub fn get(&self, key: ???) -> Option<&V>;
+    pub fn get<Q>(&self, key: &Q) -> Option<&V>
+    where
+        K: Borrow<Q>,
+        Q: Ord + ?Sized,
+    {
+        self.find(key).ok().map(|index| &self.0[index].1)
+    }
 
-    // pub fn remove(&mut self, key: ???) -> Option<V>;
+    pub fn remove<Q>(&mut self, key: &Q) -> Option<V>
+    where
+        K: Borrow<Q>,
+        Q: Ord + ?Sized,
+    {
+        self.find(key).ok().map(|index| self.0.remove(index).1)
+    }
 
-    // pub fn remove_entry(&mut self, key: ???) -> Option<(K, V)>;
+    pub fn remove_entry<Q>(&mut self, key: &Q) -> Option<(K, V)>
+    where
+        K: Borrow<Q>,
+        Q: Ord + ?Sized,
+    {
+        self.find(key).ok().map(|index| self.0.remove(index))
+    }
+
+    fn find<Q>(&self, key: &Q) -> Result<usize, usize>
+    where
+        K: Borrow<Q>,
+        Q: Ord + ?Sized,
+    {
+        self.0.binary_search_by_key(&key, |(k, _)| k.borrow())
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// impl Index for FlatMap { ... }
+impl<K, V, Q> Index<&Q> for FlatMap<K, V>
+where
+    K: Ord + Borrow<Q>,
+    Q: Ord + ?Sized,
+{
+    type Output = V;
 
-// impl Extend for FlatMap { ... }
+    fn index(&self, index: &Q) -> &Self::Output {
+        let index = self.find(index).unwrap();
+        &self.0[index].1
+    }
+}
 
-// impl From<Vec<(K, V)>> for FlatMat { ... }
+impl<K: Ord, V> Extend<(K, V)> for FlatMap<K, V> {
+    fn extend<T: IntoIterator<Item = (K, V)>>(&mut self, iter: T) {
+        let iter = iter.into_iter();
 
-// impl From<FlatMap<K, V>> for Vec<(K, V)> { ... }
+        let (count, _) = iter.size_hint();
+        self.0.reserve(count);
 
-// impl FromIterator<(K, V)> for FlatMap { ... }
+        iter.for_each(|(k, v)| {
+            self.insert(k, v);
+        })
+    }
+}
 
-// impl IntoIterator for FlatMap { ... }
+impl<K: Ord, V> From<Vec<(K, V)>> for FlatMap<K, V> {
+    fn from(value: Vec<(K, V)>) -> Self {
+        Self::from_iter(value)
+    }
+}
+
+impl<K, V> From<FlatMap<K, V>> for Vec<(K, V)> {
+    fn from(value: FlatMap<K, V>) -> Self {
+        value.0
+    }
+}
+
+impl<K: Ord, V> FromIterator<(K, V)> for FlatMap<K, V> {
+    fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+        let mut result = Self::new();
+        let iter = iter.into_iter();
+
+        let (count, _) = iter.size_hint();
+        result.0.reserve(count);
+
+        iter.for_each(|(k, v)| {
+            result.insert(k, v);
+        });
+
+        result
+    }
+}
+
+impl<K, V> IntoIterator for FlatMap<K, V> {
+    type Item = (K, V);
+
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
