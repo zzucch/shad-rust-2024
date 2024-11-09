04d5b78d056d851c84a430aff3eb0c5e179cb3ce
zzucch <nikitamilovatskiy@gmail.com>
2024-11-09 12:26:32 +0300

    feat(stdmacro): solve the task

--- task/stdmacro/src/lib.rs
+++ task/stdmacro/src/lib.rs
@@ -2,15 +2,40 @@
 
 #[macro_export]
 macro_rules! deque {
-    // TODO: your code here.
+    () => {
+        ::std::collections::VecDeque::new()
+    };
+    ($elem:expr; $n:expr) => {
+        ::std::iter::repeat($elem)
+            .take($n)
+            .collect::<::std::collections::VecDeque<_>>()
+    };
+    ($($x:expr),+ $(,)?) => {
+        [$($x),*].into_iter().collect::<::std::collections::VecDeque<_>>()
+    };
 }
 
 #[macro_export]
 macro_rules! sorted_vec {
-    // TODO: your code here.
+    () => {
+        ::std::vec::Vec::new()
+    };
+    ($elem:expr; $n:expr) => {
+        ::std::vec::from_elem($x, $n)
+    };
+    ($($x:expr),+ $(,)?) => ({
+        let mut vec = <[_]>::into_vec(
+            ::std::boxed::Box::new([$($x),+])
+        );
+        vec.sort();
+
+        vec
+    });
 }
 
 #[macro_export]
 macro_rules! map {
-    // TODO: your code here.
+    ($($key:expr=>$value:expr),* $(,)?) => {
+        ::std::collections::HashMap::from([$(($key,$value)),*])
+    };
 }
