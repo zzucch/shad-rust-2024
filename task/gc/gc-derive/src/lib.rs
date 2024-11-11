7ed576d63471c6b854c01adb42ca46133c1e1de3
zzucch <nikitamilovatskiy@gmail.com>
2024-11-11 23:17:53 +0300

    feat(gc): solve the task

--- task/gc/gc-derive/src/lib.rs
+++ task/gc/gc-derive/src/lib.rs
@@ -1,9 +1,49 @@
-use proc_macro::{Span, TokenStream};
-use quote::{quote, quote_spanned};
-use syn::{parse_macro_input, spanned::Spanned, DeriveInput, Ident};
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Scan)]
 pub fn derive_scan(input: TokenStream) -> TokenStream {
-    // TODO: your code here.
-    unimplemented!()
+    let DeriveInput {
+        ident,
+        data,
+        generics,
+        ..
+    }: DeriveInput = parse_macro_input!(input);
+
+    let fields = match data {
+        Data::Struct(struct_data) => struct_data.fields,
+        _ => panic!("`Scan` can only be derived for structs"),
+    };
+
+    let gc_collection_statements = match fields {
+        Fields::Unit => vec![],
+        Fields::Named(named_fields) => named_fields
+            .named
+            .into_iter()
+            .map(|field| {
+                let field_name = field.ident;
+                quote! {
+                    gcs.extend((&self.#field_name as &dyn Scan).collect_gcs());
+                }
+            })
+            .collect(),
+        _ => unimplemented!(),
+    };
+
+    let (impl_generics, type_generics, where_clause) = generics.split_for_impl();
+
+    let expanded = quote! {
+        impl #impl_generics Scan for #ident #type_generics #where_clause {
+            fn collect_gcs(&self) -> Vec<usize> {
+                let mut gcs = Vec::new();
+
+                #(#gc_collection_statements)*
+
+                gcs
+            }
+        }
+    };
+
+    expanded.into()
 }
--- task/gc/src/lib.rs
+++ task/gc/src/lib.rs
@@ -4,7 +4,7 @@ pub use gc_derive::Scan;
 
 use std::{
     cell::RefCell,
-    collections::{HashMap, HashSet},
+    collections::HashSet,
     marker::PhantomData,
     ops::Deref,
     rc::{Rc, Weak},
@@ -26,8 +26,10 @@ impl<T> Clone for Gc<T> {
 
 impl<T> Gc<T> {
     pub fn borrow(&self) -> GcRef<'_, T> {
-        // TODO: your code here.
-        unimplemented!()
+        GcRef {
+            rc: self.weak.upgrade().unwrap(),
+            lifetime: PhantomData,
+        }
     }
 }
 
@@ -47,40 +49,111 @@ impl<'a, T> Deref for GcRef<'a, T> {
 ////////////////////////////////////////////////////////////////////////////////
 
 pub trait Scan {
-    // TODO: your code here.
+    fn collect_gcs(&self) -> Vec<usize>;
 }
 
-// TODO: your code here.
+impl Scan for i32 {
+    fn collect_gcs(&self) -> Vec<usize> {
+        vec![]
+    }
+}
+
+impl<T> Scan for Gc<T> {
+    fn collect_gcs(&self) -> Vec<usize> {
+        vec![self.weak.as_ptr() as usize]
+    }
+}
+
+impl<T: Scan> Scan for Option<T> {
+    fn collect_gcs(&self) -> Vec<usize> {
+        match self {
+            Some(x) => x.collect_gcs(),
+            None => vec![],
+        }
+    }
+}
+
+impl<T: Scan> Scan for Vec<T> {
+    fn collect_gcs(&self) -> Vec<usize> {
+        self.iter().flat_map(Scan::collect_gcs).collect()
+    }
+}
+
+impl<T: Scan> Scan for RefCell<T> {
+    fn collect_gcs(&self) -> Vec<usize> {
+        self.borrow().collect_gcs()
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
+#[derive(Default)]
 pub struct Arena {
-    // TODO: your code here.
+    allocations: Vec<Rc<dyn Scan + 'static>>,
 }
 
 impl Arena {
     pub fn new() -> Self {
-        // TODO: your code here.
-        unimplemented!()
+        Self {
+            allocations: Vec::new(),
+        }
     }
 
     pub fn allocation_count(&self) -> usize {
-        // TODO: your code here.
-        unimplemented!()
+        self.allocations.len()
     }
 
-    pub fn alloc<T: Scan + 'static>(&mut self, obj: T) -> Gc<T> {
-        // TODO: your code here.
-        unimplemented!()
+    pub fn alloc<T: Scan + 'static>(&mut self, object: T) -> Gc<T> {
+        let allocation = Rc::new(object);
+        let gc = Gc {
+            weak: Rc::downgrade(&allocation),
+        };
+
+        self.allocations.push(allocation);
+
+        gc
     }
 
     pub fn sweep(&mut self) {
-        // TODO: your code here.
-        unimplemented!()
+        let mut internal_reference_counts = vec![0; self.allocation_count()];
+        self.allocations.iter().for_each(|allocation| {
+            allocation.collect_gcs().iter().for_each(|address| {
+                if let Some(index) = self.find_index_by_address(*address) {
+                    internal_reference_counts[index] += 1;
+                }
+            })
+        });
+
+        let mut marked = HashSet::<usize>::new();
+        self.allocations
+            .iter()
+            .enumerate()
+            .for_each(|(i, allocation)| {
+                if Rc::weak_count(allocation) > internal_reference_counts[i] {
+                    self.mark_all(Rc::as_ptr(allocation) as *const () as usize, &mut marked);
+                }
+            });
+
+        self.allocations
+            .retain(|allocation| marked.contains(&(Rc::as_ptr(allocation) as *const () as usize)));
     }
 
-    fn mark_all(&self, root_addr: usize, marked: &mut HashSet<usize>) {
-        // TODO: your code here.
-        unimplemented!()
+    fn find_index_by_address(&self, address: usize) -> Option<usize> {
+        self.allocations
+            .iter()
+            .position(|allocation| Rc::as_ptr(allocation) as *const () as usize == address)
+    }
+
+    fn mark_all(&self, root_address: usize, marked: &mut HashSet<usize>) {
+        if !marked.insert(root_address) {
+            return;
+        }
+
+        if let Some(index) = self.find_index_by_address(root_address) {
+            self.allocations[index]
+                .collect_gcs()
+                .iter()
+                .for_each(|&address| self.mark_all(address, marked));
+        }
     }
 }
